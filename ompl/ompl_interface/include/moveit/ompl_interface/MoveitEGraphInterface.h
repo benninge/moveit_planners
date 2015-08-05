e0e03b77f37c93385028db78a552b572840bd21a
benninge <benningm@informatik.uni-freiburg.de>
2015-08-05 19:57:19 +0200

    Update MoveitEGraphInterface.h

--- ompl/ompl_interface/include/moveit/ompl_interface/MoveitEGraphInterface.h
+++ ompl/ompl_interface/include/moveit/ompl_interface/MoveitEGraphInterface.h
@@ -5,7 +5,7 @@
 #include <ompl/geometric/planners/rrt/eGraphPlanner.h>
 #include <ompl/geometric/planners/rrt/BaseEGraphInterface.h>
 #include <moveit/warehouse/moveit_message_storage.h>
-#include "eGraphTraj_storage.h"
+#include "eGraph_storage.h"
 #include <moveit/robot_state/robot_state.h>
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
@@ -17,81 +17,79 @@
 #include <vector>
 #include <utility>
 
+namespace ompl_interface
+{
 
-namespace ompl_interface {
-
-class MoveitEGraphInterface: public ompl::geometric::BaseEGraphInterface {
+class MoveitEGraphInterface : public ompl::geometric::BaseEGraphInterface
+{
 public:
-    static MoveitEGraphInterface* getInstance(ModelBasedStateSpacePtr ssPtr) {
-        static MoveitEGraphInterface instance(ssPtr);
-        return &instance;
-    }
+  static MoveitEGraphInterface* getInstance(ModelBasedStateSpacePtr ssPtr)
+  {
+    static MoveitEGraphInterface instance(ssPtr);
+    return &instance;
+  }
 
-    virtual ~MoveitEGraphInterface();
+  virtual ~MoveitEGraphInterface();
 
-    virtual std::vector<ompl::geometric::EGraphNode*> load(
-            const ompl::base::SpaceInformationPtr &si);
+  virtual std::vector<ompl::geometric::EGraphNode*> load(const ompl::base::SpaceInformationPtr &si);
 
-    virtual void save(std::vector<ompl::geometric::EGraphNode*> eGraph,
-            const ompl::base::SpaceInformationPtr &si);
-    virtual void resetEGraph() {
-            mutex.lock();
-            ROS_WARN("storage reset");
-            storage_Trajs_->reset();
-            mutex.unlock();
-    }
-    virtual void resetMarkers();
+  virtual void save(std::vector<ompl::geometric::EGraphNode*> eGraph, const ompl::base::SpaceInformationPtr &si);
+  virtual void resetEGraph()
+  {
+    //mutex.lock();
+    ROS_WARN("storage reset");
+    eGraph_storage_->reset();
+    //mutex.unlock();
+  }
+  virtual void resetMarkers();
+
+  //MoveitEGraphInterface(ModelBasedStateSpacePtr ssPtr);
 
 private:
-    void draw();
-    MoveitEGraphInterface(ModelBasedStateSpacePtr ssPtr);
+  void draw(std::vector<egraphmsg::RobotStateNode> robot_nodes);
+  MoveitEGraphInterface(ModelBasedStateSpacePtr ssPtr);
 
-    MoveitEGraphInterface(MoveitEGraphInterface const&); //dont implement
-    void operator=(MoveitEGraphInterface const&); //dont implement
+  MoveitEGraphInterface(MoveitEGraphInterface const&);
+  void operator=(MoveitEGraphInterface const&);
 
-    moveit_msgs::DisplayTrajectory omplNodesToDisplayTraj(
-            std::vector<ompl::geometric::EGraphNode*> nodes);
-    std::vector<ompl::geometric::EGraphNode*> displayTrajToOmplNodes(
-            moveit_msgs::DisplayTrajectory traj_msg,
-            const ompl::base::SpaceInformationPtr &si);
-    bool addGraphToStorage(moveit_msgs::DisplayTrajectory display_trajectory,
-            std::string name, std::string robot);
-    bool addTrajToEGraph(moveit_msgs::DisplayTrajectory input_trajectory,
-            std::string graph, std::string robot);
-    moveit_msgs::DisplayTrajectory getGraphFromStorage(std::string name,
-            std::string robot);
+  std::vector<egraphmsg::RobotStateNode> omplNodesToRobotStateNodes(std::vector<ompl::geometric::EGraphNode*> nodes);
+  std::vector<ompl::geometric::EGraphNode*> robotStateNodesToOmplNodes(
+      std::vector<egraphmsg::RobotStateNode> robot_nodes, const ompl::base::SpaceInformationPtr &si);
+  bool addGraphToStorage(std::vector<egraphmsg::RobotStateNode> input_nodes, std::string name, std::string robot);
+  std::vector<egraphmsg::RobotStateNode> getGraphFromStorage(std::string name, std::string robot);
+  void drawEdge(egraphmsg::RobotStateNode node1, egraphmsg::RobotStateNode node2, int color_scheme);
 
-    void eGraphToMarkerArray(moveit_msgs::DisplayTrajectory display_trajectory,
-            robot_model::RobotModelConstPtr &robot_model, int color_scheme);
-    void trajToMarkerArray(moveit_msgs::RobotTrajectory trajectory,
-            robot_model::RobotModelConstPtr &robot_model, int color_scheme);
-    void nodeToMarkerArray(double x, double y, double z, float red, float green,
-            float blue, float alpha);
-    void publishMarkerArray(visualization_msgs::MarkerArray mA) {
-        while (markerArray_pub_.getNumSubscribers() < 1) {
-            ROS_WARN("Please create a subscriber to visualization_marker_array");
-            sleep(1);
-            ROS_WARN_ONCE("Subscriber found");
-        }
-        markerArray_pub_.publish(mA);
+  void robotNodesToMarkerArray(std::vector<egraphmsg::RobotStateNode> robot_nodes, int color_scheme);
+  void robotNodeToMarkerArray(egraphmsg::RobotStateNode node, int color_scheme);
+  void nodeToMarkerArray(double x, double y, double z, float red, float green, float blue, float alpha);
+  void publishMarkerArray(visualization_msgs::MarkerArray mA)
+  {
+    while (markerArray_pub_.getNumSubscribers() < 1)
+    {
+      ROS_WARN_ONCE("Please create a subscriber to visualization_marker_array");
+      sleep(0.01);
     }
-    geometry_msgs::Pose transformPose(
-            const Eigen::Affine3d end_effector_state) {
-        geometry_msgs::Pose pose;
-        tf::poseEigenToMsg(end_effector_state, pose);
-        return pose;
-    }
+    ROS_WARN_ONCE("Subscriber found");
+    markerArray_pub_.publish(mA);
+  }
+  geometry_msgs::Pose transformPose(const Eigen::Affine3d end_effector_state)
+  {
+    geometry_msgs::Pose pose;
+    tf::poseEigenToMsg(end_effector_state, pose);
+    return pose;
+  }
 
-    EGraphTrajStorage* storage_Trajs_;
+  EGraphStorage* eGraph_storage_;
 
-    ros::NodeHandle nh_;
-    //TODO: change to improve threaded behaviour?
-    visualization_msgs::MarkerArray mA_;
-    ros::Publisher markerArray_pub_;
-    int id_;
-    ModelBasedStateSpacePtr ssPtr_;
-   boost::mutex mutex;
-   boost::mutex resetMarkers_mutex;
+  ros::NodeHandle nh_;
+  //TODO: change to improve threaded behaviour?
+  visualization_msgs::MarkerArray mA_;
+  ros::Publisher markerArray_pub_;
+  int id_;
+  ModelBasedStateSpacePtr ssPtr_;
+  boost::mutex mutex;
+  boost::mutex resetMarkers_mutex;
+  //robot_model::RobotModelConstPtr robot_model_;
 };
 
 }
